f4ed5b3a20cf648ee984f21ac71e9b92b2e7026c
Inon Guetta <inon16guetta@gmail.com>
2025-09-07 16:44:14 +0300

    fake_data_and_loading_posts

--- finish_proj/src/App.tsx
+++ finish_proj/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import PostUsers from './components/pages/PostUsers'
+import Home from './components/pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <PostUsers/>
+      <main>
+        <Home/>
+      </main>
     </>
   )
 }
