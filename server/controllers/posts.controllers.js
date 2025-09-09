356757b1cbd0e32cf2452fa98d1ad265e0a4db98
Inon Guetta <inon16guetta@gmail.com>
2025-09-09 16:32:10 +0300

    finish_level_4_lacks_static

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,29 +1,35 @@
-import { readFile } from 'node:fs/promises';
+import { toRead, toAdd, toReadOne } from '../service/middle.service.js';
 
 export async function getAllPosts(req, res) {
     try {
-        const content = await readFile("./DB/dbPosts.txt", 'utf8');
+        const content = await toRead();
         const data = JSON.parse(content);
         res.json(data);
     } catch (e) {
-        console.error("Error getting riddles:", e.message);
+        console.error("the error it's: ", e.message);
         res.status(500).json({ msg: "Server error. Please try again later." });
-    }
-}
-
-export async function getOnePosts(req, res) {
-    try {
-        
-    } catch (e) {
-
     }
 }
 
 export async function addPost(req, res) {
     try {
-
+        const newdata = req.body;
+        const data = await toAdd(newdata);
+        res.json(data);
     } catch (e) {
+        console.error("the error it's: ", e.message);
+        res.status(500).json({ msg: "Server error. Please try again later." });
+    }
+}
 
+export async function getOnePost(req, res) {
+    try {
+        const id = req.params.id_message
+        const oneData = await toReadOne(id)
+        res.json(oneData)
+    } catch (e) {
+        console.error("server crash because: ", e.message);
+        res.status(500).json({ msg: "Server error. Please try again later." });
     }
 }
 
--- server/router/post.routes.js
+++ server/router/post.routes.js
@@ -1,12 +1,10 @@
 import express from "express";
-import { getAllPosts, getOnePosts, addPost, deletePost, updatePost } from "../controllers/posts.controllers.js";
+import { getAllPosts, getOnePost, addPost } from "../controllers/posts.controllers.js";
 
 const router = express.Router();
 
 router.get("/all-posts", getAllPosts);
-router.get("/one-post", getOnePosts);
+router.get("/one-post/:id_message", getOnePost);
 router.post("/add-post", addPost);
-router.delete("/delete-post/:id", deletePost);
-router.put("/update-post/:id", updatePost);
 
 export default router;
